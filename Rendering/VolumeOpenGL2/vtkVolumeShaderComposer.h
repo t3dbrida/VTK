a3c77980458e0af3330e2d9e19d251cdb85a1e62
Pavel Perina <perina@t3d.team>
2019-11-12 16:17:25 +0100

    Shading depends on gradient, shader is (hopefully) parametric

--- Rendering/VolumeOpenGL2/vtkVolumeShaderComposer.h
+++ Rendering/VolumeOpenGL2/vtkVolumeShaderComposer.h
@@ -219,6 +219,7 @@ namespace vtkvolume
       "uniform vec3 in_ambient[" << numInputs << "];\n"
       "uniform vec3 in_specular[" << numInputs << "];\n"
       "uniform float in_shininess[" << numInputs << "];\n"
+      "uniform vec2 in_shadingGradientScales;\n"
       "\n"
       "// Others\n"
       "uniform bool in_useJittering;\n"
@@ -687,11 +688,9 @@ namespace vtkvolume
           \n  // for now as it is causing the old mapper tests to fail\
           \n  //finalColor.xyz = in_ambient[idx] * color.rgb +\
           \n  //                 diffuse + specular;\
-          \n  float shadingFactor = smoothstep(0.10, 0.40, gradient.w);\
-          \n  finalColor.xyz = mix(color.rgb, in_ambient[idx] * color.rgb, shadingFactor); // apply color for sf=0, ambient for sf=1\
-          \n  if (shadingFactor > 0.0) {\
-          \n    finalColor.xyz += shadingFactor * (diffuse + specular);\
-          \n  }\
+          \n  float shadingFactor = smoothstep(in_shadingGradientScales[0], in_shadingGradientScales[1], gradient.w);\
+          \n  finalColor.xyz  = mix(color.rgb, in_ambient[idx] * color.rgb, shadingFactor); // apply color for sf=0, ambient for sf=1\
+          \n  finalColor.xyz += shadingFactor * (diffuse + specular);\
           \n"
           );
       }
