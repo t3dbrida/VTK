162b8570a1dff091ac6c79398c12d4ca2b621a95
wilczak <wilczak@t3d.team>
2023-01-27 09:58:26 +0100

    CMake fix?

--- CMake/vtkTargetLinkLibrariesWithDynamicLookup.cmake
+++ CMake/vtkTargetLinkLibrariesWithDynamicLookup.cmake
@@ -48,6 +48,7 @@ function(_vtkCheckUndefinedSymbolsAllowed)
 
     file(WRITE "${test_project_dir}/CMakeLists.txt"
 "
+cmake_minimum_required(VERSION 2.8.5)
 project(undefined C)
 add_library(foo SHARED \"foo.c\")
 ")
